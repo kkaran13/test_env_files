167175c4e42df133bd5cf5319cadf2fba5964bc4
kkaran13 <kmodi7542@gmail.com>
2025-11-03 13:42:51 +0530

    deleted: DEPLOYMENT_DOCUMENT.md

--- test.cs
+++ test.cs
@@ -5,8 +5,8 @@ public class EnvTest {
         // ✅ True positives
         string apiKey = Environment.GetEnvironmentVariable("API_KEY");
         string dbPass = Configuration["DB_PASS"];
-        string dbUser = Configuration["DB_USER"];
-        string dbAdmin = Configuration["DB_ADMIN"];
+        // string dbUser = Configuration["DB_USER"];
+        // string dbAdmin = Configuration["DB_ADMIN"];
 
         // ❌ False positives
         string encoding = "UTF8";
