19ddc85dfc4e1e91330a0ec1b810bbb5576213be
kkaran13 <kmodi7542@gmail.com>
2025-11-03 10:31:26 +0530

    update: test.php

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ $apiKey = getenv("API_KEY");
 $dbPass = $_ENV["DB_PASS"];
 $jwt = env("JWT_SECRET");
 $jwtKey = env("JWT_KEY");
+$admindbPass = $_ENV["ADMIN_DB_PASS"];
 
 // ❌ False positives
 $encoding = "utf-8";
